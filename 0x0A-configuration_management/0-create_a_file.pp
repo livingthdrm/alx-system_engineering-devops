0f49562ed678690ac7f8e0671d598a39c155476c
livingthdrm <salaonjoseph@gmail.com>
2024-05-19 10:32:57 -0700

    fix puppet code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,18 +1,9 @@
 #Create a manifest to create a file in /tmp
 
 file {'/tmp/school':
-<<<<<<< HEAD
   ensure  => file,
   content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
-=======
-  ensure   => 'file',
-  path     => '/tmp/school',
-  content  => 'I love Puppet',
-  owner    => 'www-data',
-  group    => 'www-data',
-  mode     => '0744'
->>>>>>> d5ccadca7772d30d0cda0b8008474a5f51313afb
+  mode    => 0744,
 }
